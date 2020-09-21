e07fd8f5e13ae3cd5e416e37c3f767af300ccbf7
Sauravupd <upd6196@gmail.com>
2020-09-21 14:54:12 +0545

    added boards

--- discussionforum/boards/tests.py
+++ discussionforum/boards/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from django.urls import reverse,resolve
+from .views import home
 
-# Create your tests here.
+class HomeTests(TestCase):
+
+    def test_home_view_status_code(self):
+
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_view_resolve(self):
+        view=resolve("/")
+        self.assertEquals(view.func,home)
+    
+    def url_status_code(self):
+        url=reverse('board_topics',kwargs={'id':1})
+        response=self.client.get(url)
+        self.assertEquals(response.status_code,200)
+    
+    def url_status_code_not_found(self):
+        url=reverse('board_topics',kwargs={'id':99})
+        response=self.client.get(url)
+        self.assertEquals(response.status_code,400)
--- discussionforum/boards/views.py
+++ discussionforum/boards/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Board
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Hello world')
+def home(request,*args, **kwargs):
+    board=Board.objects.all()
+    my_context={'board':board}
+    return render(request,'home.html',my_context)
+
+def board_topics(request,id):
+    try:
+        board=get_object_or_404(Board,id=id)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request,'topics.html',{'board':board})
